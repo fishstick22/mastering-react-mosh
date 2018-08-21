784580872988e6c7a53daa915e52e92bfe9d9812
Jeff <jeff.burkholder@gmail.com>
2018-08-21 09:51:54 -0500

    hello world

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const element = <h1>Hello World!</h1>
+console.log(element);
+ReactDOM.render(element, document.getElementById('root'));
