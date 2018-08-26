c8b3b29b2e45353802a4f2a1e9a6b561a03d64b1
Jeff <jeff.burkholder@gmail.com>
2018-08-25 23:32:01 -0500

    embedding expressions setting attributes

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1> ; 
-    }
+  state = {
+    count: 0
+  }
+
+  styles = {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <span style={this.styles} className='badge badge-primary m-2'>
+          {this.formatCount()}
+        </span>
+        <button className='btn btn-secondary btn-sm'>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
 }
- 
+
 export default Counter;
