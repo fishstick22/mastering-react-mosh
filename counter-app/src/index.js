8aaa32311a924876055b5e7dbb3b81e1a3979579
Jeff <jeff.burkholder@gmail.com>
2018-08-25 23:08:42 -0500

    bootstrap

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
 registerServiceWorker();
