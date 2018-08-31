5deb2d9f213fb0df67fed23ee8479e176f42c7cd
Jeff <jeff.burkholder@gmail.com>
2018-08-31 12:30:39 -0500

    building the movie component

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Movies from './components/movies';
 
 class App extends Component {
   render() {
     return (
       <main className='container'>
-        <h1>Hello World</h1>
+        <Movies />
       </main>
     );
   }
---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map(movie => (
+            <tr>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movies;
