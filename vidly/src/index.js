904414f650cf171630fb8e7e5843cb638e7324e6
Jeff <jeff.burkholder@gmail.com>
2018-08-31 11:38:13 -0500

    vidly project setup 2

--- vidly/src/index.js
+++ vidly/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
