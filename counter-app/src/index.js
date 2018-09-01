acfa46de1c81173d3b706b4ac131cd10350d21a9
Jeff <jeff.burkholder@gmail.com>
2018-09-01 11:43:46 -0500

    composing components

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import Counters from './components/counters';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Counters />, document.getElementById('root'));
 registerServiceWorker();
