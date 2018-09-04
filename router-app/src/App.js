ae4fd7243021cfffcf4a0c1d21c2607aacbef732
Jeff <jeff.burkholder@gmail.com>
2018-09-04 10:42:09 -0500

    passing props

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -16,7 +16,10 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route path="/products" component={Products} />
+            <Route
+              path="/products"
+              render={props => <Products sortBy="newest" {...props} />}
+            />
             <Route path="/posts" component={Posts} />
             <Route path="/admin" component={Dashboard} />
             <Route path="/" component={Home} />
