5638129d79a251cbdb5a1c70dc710b1f121dd789
Jeff <jeff.burkholder@gmail.com>
2018-09-04 12:49:01 -0500

    query string parameters

--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import queryString from 'query-string';
 
-const Posts = ({match}) => {
+const Posts = ({ match, location }) => {
+  const { sortBy} = queryString.parse(location.search);
+  console.log(queryString.parse(location.search));
+
   return (
     <div>
       <h1>Posts</h1>
