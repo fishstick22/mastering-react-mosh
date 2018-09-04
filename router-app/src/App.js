46ea3ae1ffcb843ca80068d90cce39bae910469e
Jeff <jeff.burkholder@gmail.com>
2018-09-04 10:25:17 -0500

    switch

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -15,10 +15,12 @@ class App extends Component {
       <div>
         <NavBar />
         <div className="content">
-          <Route path='/products' component={Products} />
-          <Route path='/posts' component={Posts} />
-          <Route path='/admin' component={Dashboard} />
-          <Route path='/' component={Home} />
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
         </div>
       </div>
     );
