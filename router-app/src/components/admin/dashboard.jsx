8e9826943010394281394c89e91df9f28a6a6c63
Jeff <jeff.burkholder@gmail.com>
2018-09-04 14:20:21 -0500

    nested routing

--- router-app/src/components/admin/dashboard.jsx
+++ router-app/src/components/admin/dashboard.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Sidebar from './sidebar';
+import Users from './users';
+import Posts from './posts';
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <Sidebar />
+      <Route path="/admin/users" component={Users} />
+      <Route path="/admin/posts" component={Posts} />
+
     </div>
   );
 };
