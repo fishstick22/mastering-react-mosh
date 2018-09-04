af90b4de6bdcdc805202b9fd96baec6587a92939
Jeff <jeff.burkholder@gmail.com>
2018-09-04 10:22:07 -0500

    add routing

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route } from 'react-router-dom';
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -13,6 +14,12 @@ class App extends Component {
     return (
       <div>
         <NavBar />
+        <div className="content">
+          <Route path='/products' component={Products} />
+          <Route path='/posts' component={Posts} />
+          <Route path='/admin' component={Dashboard} />
+          <Route path='/' component={Home} />
+        </div>
       </div>
     );
   }
