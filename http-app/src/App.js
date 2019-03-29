196ef3473bf61abe064ff4a537bdf71d58091384
Jeff <jeff.burkholder@gmail.com>
2019-03-29 11:44:39 -0500

    http-app start

---
+++ http-app/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  handleAdd = () => {
+    console.log("Add");
+  };
+
+  handleUpdate = post => {
+    console.log("Update", post);
+  };
+
+  handleDelete = post => {
+    console.log("Delete", post);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button className="btn btn-primary" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map(post => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ http-app/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
